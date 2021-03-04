f9a692c48954b69ac0f807600496aa6a9367e723
chaicopadillag <chaicopadillag@gmail.com>
2021-03-04 10:57:04 -0500

    Creando Build de la App

--- src/CalendarioApp.js
+++ src/CalendarioApp.js
@@ -2,14 +2,11 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 import AppRouter from './routers/AppRouter';
-import { HashRouter } from 'react-router-dom';
 
 const CalendarioApp = () => {
 	return (
 		<Provider store={store}>
-			<HashRouter>
-				<AppRouter />
-			</HashRouter>
+			<AppRouter />
 		</Provider>
 	);
 };
